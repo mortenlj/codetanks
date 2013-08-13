4b811b6e65366acd2683713b1283ab2f7f1709f4
fimojoha <fimojoha@localhost>
2013-08-13 09:10:57 +0200

    Created initial setup.py. Needs more work, and newer distribute.

---
+++ viewer/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from distribute_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
+
+import re
+
+def read(filename):
+    f = open(filename)
+    contents = f.read()
+    f.close()
+    return contents
+
+def parse_requirements(file_name):
+    requirements = []
+    for line in read(file_name).split('\n'):
+        if re.match(r'(\s*#)|(\s*$)', line):
+            continue
+        if re.match(r'\s*-e\s+', line):
+            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
+        elif re.match(r'\s*-f\s+', line):
+            pass
+        else:
+            requirements.append(line)
+    return requirements
+
+
+setup(
+    name = "Codecreeps Viewer",
+    version = "0.1",
+    packages = ["ibidem", "ibidem.codecreeps", "ibidem.codecreeps.viewer"],
+    install_requires=parse_requirements("requirements.txt"),
+    namespace_packages = ["ibidem", "ibidem.codecreeps"],
+    zip_safe = True,
+
+    # Metadata
+    author = "Morten Lied Johansen",
+    author_email = "mortenjo@ifi.uio.no",
+    license = "LGPL",
+    keywords = "ibidem codecreeps",
+    url = "TODO",
+
+    # Entry points
+    entry_points = {
+        "gui_scripts": [
+            "codecreeps-viewer = ibidem.codecreeps.viewer.main:main"
+        ],
+    }
+)
