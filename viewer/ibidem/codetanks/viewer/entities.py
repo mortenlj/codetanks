fa7209e91a0d613897615f99de13ed65d7d2e117
fimojoha <fimojoha@localhost>
2013-08-14 17:44:14 +0200

    Extract superclass MovingEntity

--- viewer/ibidem/codetanks/viewer/entities.py
+++ viewer/ibidem/codetanks/viewer/entities.py
@@ -3,29 +3,26 @@
 
 import pkg_resources
 import pygame
+from pygame.sprite import Sprite
 
 from ibidem.codetanks.viewer.vec2d import vec2d
 
 
-class Bullet(pygame.sprite.Sprite):
-    """A bullet that moves forward until it hits something"""
-    image_name = pkg_resources.resource_filename("ibidem.codetanks.viewer.resources", 'bullet_grey.png')
-    speed = 0.2
+class MovingEntity(Sprite):
+    """A moving entity.
+
+    Subclass must implement _update_visauls end provide attributes speed and image_name
+    """
+    speed = 0.1
+    image_name = ""
 
     def __init__(self, init_pos, init_dir):
-        super(Bullet, self).__init__()
+        super(MovingEntity, self).__init__()
         self.position = vec2d(init_pos)
         self.direction = vec2d(init_dir).normalized()
         self.base_image = pygame.image.load(self.image_name).convert_alpha()
         self.image = pygame.transform.rotate(self.base_image, -self.direction.angle)
         self._update_visuals()
-
-    def _update_visuals(self):
-        self.image_w, self.image_h = self.image.get_size()
-        self.rect = self.image.get_rect().move(
-            self.position.x - self.image_w / 2,
-            self.position.y - self.image_h / 2
-        )
 
     def update(self, time_passed):
         displacement = vec2d(
@@ -36,6 +33,21 @@ class Bullet(pygame.sprite.Sprite):
         self.position += displacement
         self._update_visuals()
 
+    def _update_visuals(self):
+        raise NotImplementedError()
+
+
+class Bullet(MovingEntity):
+    """A bullet that moves forward until it hits something"""
+    image_name = pkg_resources.resource_filename("ibidem.codetanks.viewer.resources", 'bullet_grey.png')
+    speed = 0.2
+
+    def _update_visuals(self):
+        self.image_w, self.image_h = self.image.get_size()
+        self.rect = self.image.get_rect().move(
+            self.position.x - self.image_w / 2,
+            self.position.y - self.image_h / 2
+        )
 
 
 if __name__ == "__main__":
