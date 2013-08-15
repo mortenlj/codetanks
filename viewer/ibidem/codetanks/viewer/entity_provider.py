22f8b1149e634af5bd7a26f6964617a1717e89e5
Morten Lied Johansen <mortenjo@ifi.uio.no>
2013-08-15 07:44:08 +0200

    Added spinning tanks

--- viewer/ibidem/codetanks/viewer/entity_provider.py
+++ viewer/ibidem/codetanks/viewer/entity_provider.py
@@ -7,21 +7,27 @@ Placeholder for actual data provider
 
 from random import choice, randint
 import pygame
-from entities import Bullet
-import pkg_resources
+from entities import Bullet, Tank
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 
 bullets = pygame.sprite.RenderUpdates()
+tanks = pygame.sprite.RenderUpdates()
+
 
 def init():
     for i in range(10):
         position = (randint(0, SCREEN_WIDTH), randint(0, SCREEN_HEIGHT))
         direction = (choice([-1, 1]), choice([-1, 1]))
         bullets.add(Bullet(position, direction))
+    for i in range(10):
+        position = (randint(0, SCREEN_WIDTH), randint(0, SCREEN_HEIGHT))
+        direction = (choice([-1, 1]), choice([-1, 1]))
+        tanks.add(Tank(position, direction))
+
 
 def get():
-    return bullets
+    return bullets, tanks
 
 if __name__ == "__main__":
     pass
