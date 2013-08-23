47b00be0589d340ec3b9163bd1b313ba97fcbf4a
Morten Lied Johansen <mortenjo@ifi.uio.no>
2013-08-23 22:18:50 +0200

    Creating separate server

---
+++ server/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+import re
+
+def read(filename):
+    f = open(filename)
+    contents = f.read()
+    f.close()
+    return contents
+
+def parse_requirements(file_name):
+    requirements = []
+    for line in read(file_name).split('\n'):
+        if re.match(r'(\s*#)|(\s*$)', line):
+            continue
+        if re.match(r'\s*-e\s+', line):
+            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
+        elif re.match(r'\s*-f\s+', line):
+            pass
+        else:
+            requirements.append(line)
+    return requirements
+
+
+setup(
+    name = "Codetanks Server",
+    version = "0.1",
+    packages = ["ibidem", "ibidem.codetanks", "ibidem.codetanks.server"],
+    install_requires=parse_requirements("requirements.txt"),
+    namespace_packages = ["ibidem", "ibidem.codetanks"],
+    zip_safe = True,
+
+    # Metadata
+    author = "Morten Lied Johansen",
+    author_email = "mortenjo@ifi.uio.no",
+    license = "LGPL",
+    keywords = "ibidem codetanks",
+    url = "https://bitbucket.org/mortenlj/codetanks",
+
+    # Entry points
+    entry_points = {
+        "console_scripts": [
+            "codetanks = ibidem.codetanks.server.main:main"
+        ],
+    }
+)
