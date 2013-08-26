cd586f4947696de176f68e32d700f6ffb149a253
fimojoha <fimojoha@localhost>
2013-08-26 09:02:43 +0200

    Clean up client-side entities

--- viewer/ibidem/codetanks/viewer/entities.py
+++ viewer/ibidem/codetanks/viewer/entities.py
@@ -61,7 +61,6 @@ class MovingEntity(pygame.sprite.Sprite):
         }
 
     def update(self, time_passed):
-        self.update_vector(time_passed)
         displacement = vec2d(
             self.direction.x * self.speed * time_passed,
             self.direction.y * self.speed * time_passed
@@ -70,9 +69,6 @@ class MovingEntity(pygame.sprite.Sprite):
         self.update_visuals()
         if not self.bounds.contains(self.rect):
             self.on_wall_collision()
-
-    def update_vector(self, time_passed):
-        raise NotImplementedError()
 
     def update_visuals(self):
         raise NotImplementedError()
@@ -85,9 +81,6 @@ class Bullet(MovingEntity):
     """A bullet that moves forward until it hits something"""
     image_name = pkg_resources.resource_filename("ibidem.codetanks.viewer.resources", 'bullet_grey.png')
     speed = 0.2
-
-    def update_vector(self, time_passed):
-        pass
 
     def update_visuals(self):
         self.image = pygame.transform.rotate(self.base_image, -self.direction.angle)
@@ -104,9 +97,6 @@ class Bullet(MovingEntity):
 class Tank(MovingEntity):
     image_name = pkg_resources.resource_filename("ibidem.codetanks.viewer.resources", "mockup_tank1.png")
     speed = 0.1
-
-    def update_vector(self, time_passed):
-        pass
 
     def update_visuals(self):
         self.image = pygame.transform.rotate(self.base_image, -self.direction.angle)
