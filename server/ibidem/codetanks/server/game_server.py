1756f7b14056c508e498e055898edf407e5f89ce
Morten Lied Johansen <mortenjo@ifi.uio.no>
2013-08-28 08:37:39 +0200

    Draw tanks inside arena

--- server/ibidem/codetanks/server/game_server.py
+++ server/ibidem/codetanks/server/game_server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
-from random import randint, choice
+from random import randint
 import pygame
 from pygame.sprite import Sprite
 from ibidem.codetanks.server.entities import Tank
@@ -13,15 +13,21 @@ class GameServer(object):
         self.bullets = pygame.sprite.RenderUpdates()
         self.bounds = pygame.Rect(0, 0, 500, 500)
         self.clock = None
-        for i in range(10):
-            position = (randint(0, self.bounds.width), randint(0, self.bounds.height))
-            direction = (choice([-1, 1]), choice([-1, 1]))
-            self.tanks.add(Tank(position, direction, self.bounds))
+        self._add_random_tank()
+
+    def _add_random_tank(self):
+        position = (randint(32, self.bounds.width - 32), randint(32, self.bounds.height - 32))
+        direction = (randint(-5, 5), randint(-5, 5))
+        while direction == (0, 0):
+            direction = (randint(-5, 5), randint(-5, 5))
+        self.tanks.add(Tank(position, direction, self.bounds))
 
     def update(self):
         if self.clock is None:
             self.clock = pygame.time.Clock()
         time_passed = self.clock.tick(50)
+        if randint(0, 50) == 0:
+            self._add_random_tank()
         self.tanks.update(time_passed)
 
     def build_game_data(self):
--- viewer/ibidem/codetanks/viewer/widgets.py
+++ viewer/ibidem/codetanks/viewer/widgets.py
@@ -12,19 +12,20 @@ class Arena(object):
     def __init__(self, width, height):
         self.wall_image = pygame.image.load(self.wall).convert_alpha()
         self.ground_image = pygame.image.load(self.ground).convert_alpha()
-        self.surface = pygame.Surface((width + (2*self.wall_image.get_width()), height + (2*self.wall_image.get_height())))
-        gwidth = self.ground_image.get_width()
-        gheight = self.ground_image.get_height()
-        for x in range(0, self.get_width(), gwidth):
-            for y in range(0, self.get_height(), gheight):
+        self.sprite_size = self.ground_image.get_width()
+        self.surface = pygame.Surface((width + (2*self.sprite_size), height + (2* self.sprite_size)))
+        self.game_field = self.surface.subsurface((self.sprite_size, self.sprite_size, width, height))
+        for x in range(0, self.get_width(), self.sprite_size):
+            for y in range(0, self.get_height(), self.sprite_size):
                 image = self.ground_image
                 if y == 0 or x == 0:
                     image = self.wall_image
                 self.surface.blit(image, (x, y))
-        for x in range(0, self.get_width(), gwidth):
-            self.surface.blit(self.wall_image, (x, self.get_height()-gheight))
-        for y in range(0, self.get_height(), gheight):
-            self.surface.blit(self.wall_image, (self.get_width()-gwidth, y))
+        for x in range(0, self.get_width(), self.sprite_size):
+            self.surface.blit(self.wall_image, (x, self.get_height()- self.sprite_size))
+        for y in range(0, self.get_height(), self.sprite_size):
+            self.surface.blit(self.wall_image, (self.get_width()- self.sprite_size, y))
+        self.background = self.game_field.copy()
 
     def draw(self, target, dest):
         target.blit(self.surface, dest)
