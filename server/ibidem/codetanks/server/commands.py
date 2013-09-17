47f09ec0ea5481711a3bbd4fafd669ef627b3b7e
Morten Lied Johansen <mortenjo@ifi.uio.no>
2013-09-17 22:36:48 +0200

    Minor changes to logging etc.

--- server/ibidem/codetanks/server/commands.py
+++ server/ibidem/codetanks/server/commands.py
@@ -56,27 +56,22 @@ class Move(Command):
             self.tshape.collision_type = self.tank.collision_type + 10
             self.constraint = pymunk.DampedSpring(self.tank.body, self.target, (0, 0), (0, 0), 0, 2.0, 2.0)
             to_add = (self.tshape, self.constraint)
-            self.log("Adding %s to space" % ", ".join((repr(x) for x in to_add)))
             self.space.add(*to_add)
             self.log("Adding collision handler %r => %r" % (self.tank.collision_type, self.tshape.collision_type))
             self.space.add_collision_handler(self.tank.collision_type, self.tshape.collision_type, self.on_collision)
             self._started = True
 
     def on_collision(self, space, arbiter, *args, **kwargs):
+        self.log("Trying to stop %s" % self.tank)
         self.tank.body.reset_forces()
         self.tank.body.velocity = (0, 0)
         to_remove = [self.tshape]
         to_remove.extend(self.tank.body.constraints)
-        self.log("Removing %s from space" % ", ".join((repr(x) for x in to_remove)))
         space.remove(*to_remove)
         self._finished = True
         return False
 
     def _finished_internal(self):
-        self.log("Current force: %r, torque: %r, velocity: %r" % (
-            self.tank.body.force,
-            self.tank.body.torque,
-            self.tank.body.torque))
         if self._finished:
             self.log("Removing collision handler %r => %r" % (self.tank.collision_type, self.tshape.collision_type))
             self.space.remove_collision_handler(self.tank.collision_type, self.tshape.collision_type)
