6f87ce65da0b07eaef1e1f89184157e5b351e89b
Morten Lied Johansen <mortenjo@ifi.uio.no>
2014-04-30 23:00:15 +0200

    Extending broker with more features

--- server/ibidem/codetanks/server/broker.py
+++ server/ibidem/codetanks/server/broker.py
@@ -4,7 +4,7 @@
 from socket import gethostname
 
 from pinject import copy_args_to_public_fields
-import zmq
+import zmq.green as zmq
 
 
 class Socket(object):
@@ -15,6 +15,12 @@ class Socket(object):
     @property
     def url(self):
         return "tcp://%s:%d" % (gethostname(), self.port)
+
+    def recv_json(self):
+        return self.zmq_socket.recv_json()
+
+    def send_json(self, value):
+        self.zmq_socket.send_json(value)
 
 
 def create_socket(zmq_context, socket_type, port):
@@ -27,12 +33,26 @@ def create_socket(zmq_context, socket_type, port):
 
 
 class Broker(object):
-    def __init__(self, zmq_context, zmq_poller, registration_port=None):
+    def __init__(self, zmq_context, zmq_poller, game_server_channel, registration_port=None):
         self.registration_socket = create_socket(zmq_context, zmq.REP, registration_port)
         self.update_socket = create_socket(zmq_context, zmq.PUB, None)
         self.zmq_poller = zmq_poller
         self.zmq_poller.register(self.registration_socket)
         self.zmq_poller.register(self.update_socket)
+        self.game_server_channel = game_server_channel
+
+    def run(self):
+        while True:
+            self._run_once()
+
+    def _run_once(self):
+        socks = self.zmq_poller.poll(1)
+        for pair in socks:
+            if pair == (self.registration_socket.zmq_socket, zmq.POLLIN):
+                event = self.registration_socket.recv_json()
+                self.registration_socket.send_json({"update_url": self.update_socket.url})
+                self.game_server_channel.send({"event": "registration", "id": event["id"]})
+
 
 if __name__ == "__main__":
     pass
--- server/tests/test_broker.py
+++ server/tests/test_broker.py
@@ -3,7 +3,8 @@
 
 from mock import create_autospec, patch, call
 from nose.tools import eq_, assert_true
-import zmq
+import zmq.green as zmq
+from goless.channels import GoChannel
 
 from ibidem.codetanks.server.broker import Broker
 
@@ -16,32 +17,49 @@ class Shared(object):
     # Mocks
     zmq_context = None
     zmq_poller = None
+    game_server_channel = None
 
     def setup(self):
-        self.zmq_context = create_autospec(zmq.Context)
+        self.zmq_context = create_autospec(zmq.Context, instance=True)
         self.zmq_poller = create_autospec(zmq.Poller, instance=True)
+        self.game_server_channel = create_autospec(GoChannel, instance=True)
 
 
 class TestSockets(Shared):
     def test_are_opened(self):
-        Broker(self.zmq_context, self.zmq_poller)
+        Broker(self.zmq_context, self.zmq_poller, self.game_server_channel)
         expected_calls = [call(zmq.REP), call(zmq.PUB)]
         eq_(self.zmq_context.socket.call_args_list, expected_calls)
 
     def test_registration_is_opened_on_given_port(self):
-        broker = Broker(self.zmq_context, self.zmq_poller, self.port)
+        broker = Broker(self.zmq_context, self.zmq_poller, self.game_server_channel, self.port)
         eq_(broker.registration_socket.port, self.port)
 
     def test_has_valid_urls(self):
         with patch("ibidem.codetanks.server.broker.gethostname", return_value=self.hostname):
-            broker = Broker(self.zmq_context, self.zmq_poller, self.port)
+            broker = Broker(self.zmq_context, self.zmq_poller, self.game_server_channel, self.port)
             eq_(broker.registration_socket.url, "tcp://%s:%d" % (self.hostname, self.port))
             assert_true(broker.update_socket.url.startswith("tcp://%s" % self.hostname))
 
     def test_are_registered_with_poller(self):
-        broker = Broker(self.zmq_context, self.zmq_poller)
+        broker = Broker(self.zmq_context, self.zmq_poller, self.game_server_channel)
         expected_calls = [call(broker.registration_socket), call(broker.update_socket)]
         eq_(self.zmq_poller.register.call_args_list, expected_calls)
+
+
+class TestRegistration(Shared):
+    def test_client_gets_update_url_back(self):
+        broker = Broker(self.zmq_context, self.zmq_poller, self.game_server_channel)
+        self.zmq_poller.poll.return_value = [(broker.registration_socket.zmq_socket, zmq.POLLIN)]
+        broker._run_once()
+        broker.registration_socket.zmq_socket.send_json.assert_called_once_with({"update_url": broker.update_socket.url})
+
+    def test_client_id_is_sent_to_game_server_channel(self):
+        broker = Broker(self.zmq_context, self.zmq_poller, self.game_server_channel)
+        self.zmq_poller.poll.return_value = [(broker.registration_socket.zmq_socket, zmq.POLLIN)]
+        broker.registration_socket.zmq_socket.recv_json.return_value = {"id": "client_id"}
+        broker._run_once()
+        self.game_server_channel.send.assert_called_once_with({"event": "registration", "id": "client_id"})
 
 
 if __name__ == "__main__":
