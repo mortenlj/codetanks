0ed4e891f72da4436eb272b33ee1dcbb94094f7e
Morten Lied Johansen <mortenjo@ifi.uio.no>
2014-06-03 21:46:27 +0200

    Flesh out the command-line bot for testing/development, ready to add commands

--- server/ibidem/codetanks/server/cli_bot.py
+++ server/ibidem/codetanks/server/cli_bot.py
@@ -1,13 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
-
+from functools import wraps
+import shlex
 import argparse
+import inspect
 from cmd import Cmd
 
 import zmq
 
 from ibidem.codetanks.domain.ttypes import Registration, ClientType, Id
 from ibidem.codetanks.domain.util import serialize, deserialize
+
+
+class NoExitArgumentParser(argparse.ArgumentParser):
+    class InvalidArguments(Exception):
+        pass
+
+    def exit(self, status=0, message=None):
+        if message:
+            self._print_message(message)
+        raise NoExitArgumentParser.InvalidArguments(message)
+
+
+def parse_args(func):
+    name = func.__name__[3:]
+    args, varargs, kwargs, defaults = inspect.getargspec(func)
+    args.remove("self")
+    assert len(args) == len(defaults), "You must supply example value for all args of %s" % name
+    assert None not in defaults, "You can't use None as an example value in %s" % name
+    parser = NoExitArgumentParser(prog=name, add_help=False)
+    for name, value in zip(args, defaults):
+        parser.add_argument(name, type=type(value))
+    @wraps(func)
+    def wrapper(self, line):
+        try:
+            params = parser.parse_args(shlex.split(line))
+            call_args = [getattr(params, name) for name in args]
+            return func(self, *call_args)
+        except NoExitArgumentParser.InvalidArguments:
+            return False
+    return wrapper
 
 
 class CliBot(Cmd):
@@ -27,8 +59,10 @@ class CliBot(Cmd):
         print "Subscribing to %s" % update_url
         self._update_socket.connect(update_url)
 
-    def do_EOF(self, line):
+    def do_exit(self, line):
+        """Leave the bot-cli. Your bot will be left in whatever state it is in."""
         return True
+    do_EOF = do_exit
 
 
 def main():
