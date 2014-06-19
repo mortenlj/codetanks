5db0d1d610fc094ac263c6439c646e4fe7bd684e
Morten Lied Johansen <mortenjo@ifi.uio.no>
2014-06-19 22:57:18 +0200

    Removed broker, hidden zmq inside com.Channel. Some tests missing.

--- server/ibidem/codetanks/server/com.py
+++ server/ibidem/codetanks/server/com.py
@@ -3,17 +3,44 @@
 
 from socket import gethostname
 
+import zmq
+
 from ibidem.codetanks.domain.util import deserialize, serialize
 
 
-class _Socket(object):
-    def __init__(self, zmq_socket, port):
-        self.zmq_socket = zmq_socket
-        self.port = port
+class SocketType(object):
+    REQUEST = zmq.REQ
+    REPLY = zmq.REP
+    PUBLISH = zmq.PUB
+    SUBSCRIBE = zmq.SUB
+
+
+class Channel(object):
+    url_scheme = "tcp"
+    url_wildcard = "*"
+
+    def __init__(self, socket_type, port=None):
+        ctx = zmq.Context.instance()
+        self.zmq_socket = ctx.socket(socket_type)
+        self.port = self._bind_socket(port)
+
+    def _bind_socket(self, port):
+        if port:
+            self.zmq_socket.bind(self._create_url(self.url_wildcard, port))
+        else:
+            port = self.zmq_socket.bind_to_random_port(self._create_url(self.url_wildcard))
+        return port
+
+    def _create_url(self, host, port=None):
+        url = "%s://%s" % (self.url_scheme, host)
+        return "%s:%d" % (url, port) if port else url
 
     @property
     def url(self):
-        return "tcp://%s:%d" % (gethostname(), self.port)
+        return self._create_url(gethostname(), self.port)
+
+    def ready(self):
+        return self.zmq_socket.poll(1) == zmq.POLLIN
 
     def recv(self):
         data = self.zmq_socket.recv(copy=False)
@@ -23,15 +50,6 @@ class _Socket(object):
     def send(self, value):
         data = serialize(value)
         self.zmq_socket.send(data, copy=False)
-
-
-def create(zmq_context, socket_type, port):
-    zmq_socket = zmq_context.socket(socket_type)
-    if port:
-        zmq_socket.bind("tcp://*:%d" % port)
-    else:
-        port = zmq_socket.bind_to_random_port("tcp://*")
-    return _Socket(zmq_socket, port)
 
 if __name__ == "__main__":
     pass
