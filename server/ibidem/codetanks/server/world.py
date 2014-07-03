32827bbfab2df866914c43bda53f0d8e1b981745
Morten Lied Johansen <mortenjo@ifi.uio.no>
2014-07-03 13:27:37 +0200

    World is now responsible for GameData

--- server/ibidem/codetanks/server/world.py
+++ server/ibidem/codetanks/server/world.py
@@ -4,15 +4,17 @@
 from ibidem.codetanks.domain.ttypes import GameData, Arena
 
 
-class World(GameData):
+class World(object):
     arena = Arena(0, 0)
 
     def __init__(self, world_width, world_height):
-        super(World, self).__init__([], [])
         self.arena = Arena(world_width, world_height)
 
     def add_tank(self, bot):
         pass
+
+    def build_game_data(self):
+        return GameData([], [])
 
 if __name__ == "__main__":
     pass
