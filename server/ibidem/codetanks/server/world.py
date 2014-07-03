473db9d281d58d61d72a3c2fc68c929120c2d358
Morten Lied Johansen <mortenjo@ifi.uio.no>
2014-07-03 13:51:27 +0200

    Tanks are added to world inside arena

--- server/ibidem/codetanks/server/world.py
+++ server/ibidem/codetanks/server/world.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
 
-from ibidem.codetanks.domain.ttypes import GameData, Arena
+from ibidem.codetanks.domain.ttypes import GameData, Arena, Tank, Point
 
 
-class World(object):
+class World(GameData):
     arena = Arena(0, 0)
 
     def __init__(self, world_width, world_height):
+        super(World, self).__init__([], [])
         self.arena = Arena(world_width, world_height)
 
     def add_tank(self, bot):
+        self.tanks.append(Tank(bot.tank_id, bot.bot_id, self._select_valid_position()))
         pass
 
-    def build_game_data(self):
-        return GameData([], [])
+    def _select_valid_position(self):
+        return Point(self.arena.width / 2, self.arena.height / 2)
+
 
 if __name__ == "__main__":
     pass
--- server/tests/test_world.py
+++ server/tests/test_world.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
 
-from nose.tools import assert_is_instance, assert_equal
+from nose.tools import assert_is_instance, assert_equal, assert_greater, assert_less
 
-from ibidem.codetanks.domain.ttypes import Arena
+from ibidem.codetanks.domain.ttypes import Arena, Id, Tank
+from ibidem.codetanks.server.bot import Bot
 from ibidem.codetanks.server.world import World
 
 
 class TestWorld(object):
     width = 500
     height = 500
+    bot_id = Id("bot", 1)
 
     def test_game_data_is_initialized_with_lists(self):
         world = World(self.width, self.height)
-        game_data = world.build_game_data()
-        assert_is_instance(game_data.bullets, list)
-        assert_is_instance(game_data.tanks, list)
+        assert_is_instance(world.bullets, list)
+        assert_is_instance(world.tanks, list)
 
     def test_arena_is_given_size(self):
         world = World(self.width, self.height)
         assert_is_instance(world.arena, Arena)
         assert_equal(world.arena.width, self.width)
         assert_equal(world.arena.height, self.height)
+
+    def test_tank_is_placed_inside_arena(self):
+        world = World(self.width, self.height)
+        world.add_tank(Bot(self.bot_id, 0, None, None))
+        tank = world.tanks[0]
+        assert_is_instance(tank, Tank)
+        assert_greater(tank.position.x, 0)
+        assert_greater(tank.position.y, 0)
+        assert_less(tank.position.x, self.width)
+        assert_less(tank.position.y, self.height)
 
 
 if __name__ == "__main__":
