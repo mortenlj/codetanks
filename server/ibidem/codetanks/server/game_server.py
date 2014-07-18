1411697081739fe5c5ac88edf5ae190f58ec3e62
Morten Lied Johansen <mortenjo@ifi.uio.no>
2014-07-18 22:59:32 +0200

    Game server handles rotation command

--- server/ibidem/codetanks/server/game_server.py
+++ server/ibidem/codetanks/server/game_server.py
@@ -5,7 +5,7 @@ from functools import partial
 import pygame
 from pinject import copy_args_to_internal_fields
 
-from ibidem.codetanks.domain.ttypes import GameInfo, RegistrationReply, ClientType, Move, CommandResult, CommandReply
+from ibidem.codetanks.domain.ttypes import GameInfo, RegistrationReply, ClientType, Move, CommandResult, CommandReply, Rotate
 from ibidem.codetanks.server.bot import Bot
 from ibidem.codetanks.server.com import ChannelType
 
@@ -64,6 +64,9 @@ class GameServer(object):
     def _handle_bot_cmd(self, bot, reply_channel, command):
         if isinstance(command, Move):
             self._world.move(bot.tank_id, command.distance)
+            reply_channel.send(CommandReply(CommandResult.OK))
+        elif isinstance(command, Rotate):
+            self._world.rotate(bot.tank_id, command.angle)
             reply_channel.send(CommandReply(CommandResult.OK))
 
     def build_game_info(self):
