1c969ec1e7e0d0c779a140d86ec28ad8cbee339e
Morten Lied Johansen <mortenjo@ifi.uio.no>
2014-09-04 22:44:32 +0200

    Render dead tanks with tombstone. Improve rendering in general.

--- viewer/ibidem/codetanks/viewer/entities.py
+++ viewer/ibidem/codetanks/viewer/entities.py
@@ -4,6 +4,7 @@
 import pkg_resources
 import pygame
 
+from ibidem.codetanks.domain.ttypes import BotStatus
 from ibidem.codetanks.viewer.vec2d import vec2d
 
 
@@ -24,6 +25,7 @@ class Entity(pygame.sprite.Sprite):
 class Bullet(Entity):
     """A bullet that moves forward until it hits something"""
     image_name = pkg_resources.resource_filename("ibidem.codetanks.viewer.resources", 'bullet_grey.png')
+    _layer = 5
 
     def __init__(self, e):
         super(Bullet, self).__init__(e)
@@ -46,22 +48,30 @@ class Tank(Entity):
         pkg_resources.resource_filename("ibidem.codetanks.viewer.resources", "green_turret.png"),
         pkg_resources.resource_filename("ibidem.codetanks.viewer.resources", "yellow_turret.png")
     )
+    dead_image_name = pkg_resources.resource_filename("ibidem.codetanks.viewer.resources", "tombstone.png")
+    _layer = 10
 
     def __init__(self, e):
         super(Tank, self).__init__(e)
         self.bot_id = e.bot_id
         self.turret = vec2d(e.turret.x, e.turret.y)
         self.health = e.health
+        self.status = e.status
         self.base_body_image = pygame.image.load(self.body_image_name).convert_alpha()
         self.base_turret_image = pygame.image.load(self.turret_image_names[self.id % len(self.turret_image_names)]).convert_alpha()
+        self.dead_image = pygame.image.load(self.dead_image_name).convert_alpha()
 
     def update_visuals(self):
-        self.image = pygame.transform.rotate(self.base_body_image, -self.direction.angle)
-        self.image_w, self.image_h = self.image.get_size()
-        new_turret_image = pygame.transform.rotate(self.base_turret_image, -self.turret.angle)
-        w, h = new_turret_image.get_size()
-        rect = new_turret_image.get_rect().move((self.image_w / 2) - (w / 2), (self.image_h / 2) - (h / 2))
-        self.image.blit(new_turret_image, rect)
+        if self.status == BotStatus.DEAD:
+            self.image = self.dead_image
+            self.image_w, self.image_h = self.image.get_size()
+        else:
+            self.image = pygame.transform.rotate(self.base_body_image, -self.direction.angle)
+            self.image_w, self.image_h = self.image.get_size()
+            new_turret_image = pygame.transform.rotate(self.base_turret_image, -self.turret.angle)
+            w, h = new_turret_image.get_size()
+            rect = new_turret_image.get_rect().move((self.image_w / 2) - (w / 2), (self.image_h / 2) - (h / 2))
+            self.image.blit(new_turret_image, rect)
         self.rect = self.image.get_rect().move(
             self.position.x - self.image_w / 2,
             self.position.y - self.image_h / 2
