b92e34628bc3435d85470097debe7b947e3dafa3
Morten Lied Johansen <mortenjo@ifi.uio.no>
2015-04-01 22:39:21 +0200

    Gradle to build domain package, both java and python distributables

--- domain/setup.py
+++ domain/setup.py
@@ -1,48 +1,7 @@
 #!/usr/bin/env python
 
-import shutil
-
-from setuptools import setup, Command
-import os
+from setuptools import setup
 import re
-
-
-class Thrift(Command):
-    description = "Compile Thrift IDL into python source"
-    user_options = []
-
-    _languages = [
-        "py:new_style,utf8strings,slots"
-    ]
-
-    def initialize_options(self):
-        self.thrift_file = os.path.join("thrift", "messages.thrift")
-
-    def finalize_options(self):
-        pass
-
-    def _fixup_py(self):
-        path = "."
-        for folder in ("target", "gen-py", "ibidem", "codetanks"):
-            path = os.path.join(path, folder)
-            filename = os.path.join(path, "__init__.py")
-            if os.path.exists(filename):
-                with open(filename, "w") as fobj:
-                    fobj.write("__import__('pkg_resources').declare_namespace(__name__)\n")
-        os.unlink(os.path.join("target", "gen-py", "__init__.py"))
-
-    def run(self):
-        gen_py_path = os.path.join("target", "gen-py")
-        os.makedirs(gen_py_path)
-        shutil.rmtree(gen_py_path)
-        shutil.copytree(os.path.join("src", "python", "ibidem"), os.path.join("target", "gen-py", "ibidem"))
-        cmd_args = ["thrift-0.9.1", "-strict", "-verbose"]
-        for lang in self._languages:
-            cmd_args.append("--gen")
-            cmd_args.append(lang)
-        cmd_args.extend(["-o", "target", self.thrift_file])
-        self.spawn(cmd_args)
-        self._fixup_py()
 
 
 def read(filename):
@@ -71,12 +30,11 @@ setup(
     version="0.1",
     packages=["ibidem", "ibidem.codetanks", "ibidem.codetanks.domain"],
     package_dir={
-        '': 'target/gen-py'
+        '': 'build/python'
     },
     install_requires=parse_requirements("requirements.txt"),
     namespace_packages=["ibidem", "ibidem.codetanks"],
     zip_safe=True,
-    cmdclass={"thrift": Thrift},
 
     # Metadata
     author="Morten Lied Johansen",
