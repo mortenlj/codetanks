2c3f3ea853288a2209245162646693833cd850c8
Morten Lied Johansen <mortenjo@ifi.uio.no>
2015-06-09 22:15:09 +0200

    Use regular dependencies script block

--- buildSrc/src/main/groovy/ibidem/gradle/python/PythonPlugin.groovy
+++ buildSrc/src/main/groovy/ibidem/gradle/python/PythonPlugin.groovy
@@ -4,6 +4,9 @@ import javax.inject.Inject
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.internal.DefaultDomainObjectSet
+import org.gradle.api.internal.artifacts.DefaultDependencySet
 import org.gradle.api.internal.file.FileResolver
 
 class PythonPlugin implements Plugin<Project> {
@@ -16,13 +19,14 @@ class PythonPlugin implements Plugin<Project> {
 
     void apply(Project project) {
         project.configure(project) {
-            project.configurations.create('main')
-            project.configurations.create('develop')
+            project.configurations.create('python')
             BuildPythonTask build = project.tasks.create(BuildPythonTask.NAME, BuildPythonTask.class)
-            def python = project.extensions.create('python', Python.class, project)
 
             afterEvaluate {
-                build.dependencies = project.configurations.getByName('main').dependencies
+                def pythonDependencies = project.configurations.getByName('python')
+                        .dependencies.collect { dep -> new PythonDependency(dep.name, dep.version) }
+                def domainObjectSet = new DefaultDomainObjectSet<Dependency>(PythonDependency.class, pythonDependencies)
+                build.dependencies = new DefaultDependencySet("python dependencies", domainObjectSet)
 
                 project.tasks.getByName('assemble').dependsOn(BuildPythonTask.NAME)
             }
