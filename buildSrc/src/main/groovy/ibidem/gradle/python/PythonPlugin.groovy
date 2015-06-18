44e8094031195b6dcd00648a939c88a860af3f4c
Morten Lied Johansen <mortenjo@ifi.uio.no>
2015-06-18 22:17:59 +0200

    Read python deps from requirement-files

--- buildSrc/src/main/groovy/ibidem/gradle/python/PythonPlugin.groovy
+++ buildSrc/src/main/groovy/ibidem/gradle/python/PythonPlugin.groovy
@@ -4,16 +4,12 @@ import javax.inject.Inject
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.Dependency
-import org.gradle.api.internal.DefaultDomainObjectSet
-import org.gradle.api.internal.artifacts.DefaultDependencySet
 import org.gradle.api.internal.file.FileResolver
 
 /*
 TODO:
     - Connect test to build task?
     - If java-plugin is not applied, create necessary build and install tasks
-    - Remove dependencies in build.gradle, they should be in requirements, test_requirements and setup_requirements
     - Set more properties in generated setup.py:
         - project name
         - metadata? (Does gradle have standard metadata support?)
@@ -21,7 +17,6 @@ TODO:
 
 class PythonPlugin implements Plugin<Project> {
     private final FileResolver fileResolver
-    private File pythonBuildRoot
 
     @Inject
     PythonPlugin(FileResolver fileResolver) {
@@ -43,7 +38,6 @@ class PythonPlugin implements Plugin<Project> {
     void apply(Project project) {
         def myPaths = definePaths(project.buildDir)
         project.configure(project) {
-            project.configurations.create('python')
             CollectPython collect = project.tasks.create(CollectPython.NAME, CollectPython.class) {
                 paths = myPaths
             }
@@ -52,7 +46,7 @@ class PythonPlugin implements Plugin<Project> {
             }
             BuildPython build = project.tasks.create(BuildPython.NAME, BuildPython.class) {
                 paths = myPaths
-                dependsOn GenerateSetup.NAME
+                dependsOn GenerateSetup.NAME, CollectPython.NAME
             }
             TestPython test = project.tasks.create(TestPython.NAME, TestPython.class) {
                 dependsOn BuildPython.NAME
@@ -63,11 +57,6 @@ class PythonPlugin implements Plugin<Project> {
             }
 
             afterEvaluate {
-                def pythonDependencies = project.configurations.getByName('python')
-                        .dependencies.collect { dep -> new PythonDependency(dep.name, dep.version) }
-                def domainObjectSet = new DefaultDomainObjectSet<Dependency>(PythonDependency.class, pythonDependencies)
-                setup.dependencies = new DefaultDependencySet("python dependencies", domainObjectSet)
-
                 project.tasks.getByName('assemble').dependsOn(BuildPython.NAME)
                 project.tasks.getByName('test').dependsOn(TestPython.NAME)
                 project.tasks.getByName('install').dependsOn(InstallPython.NAME)
