7d784650a9eed7654643457ee6cf8ccd97950d23
Morten Lied Johansen <mortenjo@ifi.uio.no>
2020-07-13 21:20:21 +0200

    Cleanup

--- demo.sh
+++ demo.sh
@@ -13,11 +13,11 @@ bot_2_pid=$!
 ./gradlew runRandomizer > /tmp/randomizer3.log 2>&1 &
 bot_3_pid=$!
 sleep 1
-codetanks-viewer tcp://yeager:13337 &
+codetanks-viewer tcp://localhost:13337 &
 viewer_pid=$!
-codetanks_bot tcp://yeager:13337
+codetanks_bot tcp://localhost:13337
 
-for p in ${server_pid} ${viewer_pid} ${bot_1_pod} ${bot_2_pid} ${bot_3_pid}; do
+for p in ${server_pid} ${viewer_pid} ${bot_1_pid} ${bot_2_pid} ${bot_3_pid}; do
   kill ${p}
   sleep 2
   kill -9 ${p}
