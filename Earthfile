7da1227ff145281e2a814a5124f9e700c9b0aa9c
Morten Lied Johansen <mortenjo@ifi.uio.no>
2021-08-08 23:46:14 +0200

    Earthly build for sample-groovy

--- Earthfile
+++ Earthfile
@@ -2,3 +2,4 @@ all:
     BUILD ./server+test
     BUILD ./server+docker
     BUILD ./viewer+docker
+    BUILD ./sample-groovy+docker
--- demo.sh
+++ demo.sh
@@ -4,25 +4,12 @@ export TERM=xterm
 
 earthly +all
 
-server_c=$(docker run --net=host --rm --detach mortenlj/codetanks-server)
+docker run --label codetanks=server --net=host --rm --detach mortenlj/codetanks-server
 sleep 10
-pushd sample-groovy
-./gradlew runRandomizer > /tmp/randomizer1.log 2>&1 &
-bot_1_pid=$!
-./gradlew runRandomizer > /tmp/randomizer2.log 2>&1 &
-bot_2_pid=$!
-./gradlew runRandomizer > /tmp/randomizer3.log 2>&1 &
-bot_3_pid=$!
-sleep 1
-viewer_c=$(docker run --net=host -e DISPLAY -v ~/.Xauthority:/root/.Xauthority --rm --detach mortenlj/codetanks-viewer tcp://localhost:13337)
-docker run --net=host --rm -ti --entrypoint codetanks_bot mortenlj/codetanks-server tcp://localhost:13337
-
-for p in ${bot_1_pid} ${bot_2_pid} ${bot_3_pid}; do
-  kill ${p}
-  sleep 2
-  kill -9 ${p}
+docker run --label codetanks=viewer --net=host -e DISPLAY -v ~/.Xauthority:/root/.Xauthority --rm --detach mortenlj/codetanks-viewer tcp://localhost:13337
+for i in $(seq 1 3); do
+  docker run --label codetanks=sample-groovy-$i --net=host --rm --detach mortenlj/codetanks-sample-groovy tcp://localhost:13337
 done
+docker run --label codetanks=cli --net=host --rm -ti --entrypoint codetanks_bot mortenlj/codetanks-server tcp://localhost:13337
 
-for c in ${server_c} ${viewer_c}; do
-  docker container stop $c
-done
+docker container list --filter label=codetanks --quiet | xargs docker container stop
