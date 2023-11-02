111250083ca0c0e41a783e678ddd04cac00d40d8
Morten Lied Johansen <mortenjo@ifi.uio.no>
2023-11-02 17:46:24 +0100

    Add Rusty to demo

--- demo.sh
+++ demo.sh
@@ -8,13 +8,11 @@ docker run --label codetanks=server --name codetanks_server --net=host --rm --de
 sleep 10
 docker run --label codetanks=viewer --name codetanks_viewer --net=host -e DISPLAY -v ~/.Xauthority:/root/.Xauthority --rm --detach ghcr.io/mortenlj/codetanks-viewer tcp://localhost:13337
 
-for i in $(seq 1 4); do
-  docker run --label codetanks=sample-groovy-$i --net=host --rm --detach ghcr.io/mortenlj/codetanks-sample-groovy tcp://localhost:13337
-done
-
-#docker run --label codetanks=sample-rust --net=host --rm --detach ghcr.io/mortenlj/codetanks-sample-rust tcp://localhost:13337
-
-#docker run --label codetanks=cli --net=host --rm -ti --entrypoint codetanks_bot ghcr.io/mortenlj/codetanks-server tcp://localhost:13337
+# Launch bots
+docker run --label codetanks=sample-groovy-1 --net=host --rm --detach ghcr.io/mortenlj/codetanks-sample-groovy tcp://localhost:13337
+docker run --label codetanks=sample-groovy-2 --net=host --rm --detach ghcr.io/mortenlj/codetanks-sample-groovy tcp://localhost:13337
+docker run --label codetanks=sample-groovy-3 --net=host --rm --detach ghcr.io/mortenlj/codetanks-sample-groovy tcp://localhost:13337
+docker run --label codetanks=sample-rust --net=host --rm --detach -e SAMPLE_RUST__SERVER_URI=tcp://localhost:13337 ghcr.io/mortenlj/codetanks-sample-rust
 
 docker attach codetanks_server
 
