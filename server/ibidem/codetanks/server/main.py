1d8694316ec86aa4ffeb4d9537612d7466efb2e5
Morten Lied Johansen <mortenjo@ifi.uio.no>
2023-11-02 12:48:39 +0100

    Allow enabling debug logging without also enabling debug plots etc.

--- server/ibidem/codetanks/server/main.py
+++ server/ibidem/codetanks/server/main.py
@@ -41,9 +41,10 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--port", type=int, default=13337)
     parser.add_argument("--debug", action="store_true", help="Enable debug features")
+    parser.add_argument("--debuglogs", action="store_true", help="Enable debug logging")
     args = parser.parse_args()
     loglevel = logging.INFO
-    if args.debug:
+    if args.debug or args.debuglogs:
         loglevel = logging.DEBUG
     logging.basicConfig(level=loglevel)
     obj_graph = pinject.new_object_graph(modules=None, binding_specs=[ObjectGraph(args.port, args.debug)])
