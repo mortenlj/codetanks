9a61e3594fb01e45abac2523d0ce1ebd6fc0eb68
Morten Lied Johansen <mortenjo@ifi.uio.no>
2023-11-06 13:09:08 +0100

    Improve demo script

--- demo.sh
+++ demo.sh
@@ -8,15 +8,15 @@ export TERM=xterm
 ./earthlyw +all
 
 docker run --label codetanks=server --name codetanks_server --net=host --rm --detach ghcr.io/mortenlj/codetanks-server:latest
-sleep 10
 docker run --label codetanks=viewer --name codetanks_viewer --net=host -e DISPLAY -v ~/.Xauthority:/root/.Xauthority --rm --detach ghcr.io/mortenlj/codetanks-viewer tcp://localhost:13337
+sleep 5
 
 # Launch bots
-docker run --label codetanks=sample-groovy-1 --net=host --rm --detach ghcr.io/mortenlj/codetanks-sample-groovy tcp://localhost:13337
-docker run --label codetanks=sample-groovy-2 --net=host --rm --detach ghcr.io/mortenlj/codetanks-sample-groovy tcp://localhost:13337
-docker run --label codetanks=sample-groovy-3 --net=host --rm --detach ghcr.io/mortenlj/codetanks-sample-groovy tcp://localhost:13337
-docker run --label codetanks=sample-rust --net=host --rm --detach -e SAMPLE_RUST__SERVER_URI=tcp://localhost:13337 ghcr.io/mortenlj/codetanks-sample-rust
+docker run --label codetanks=sample-groovy-1 --name sample-groovy-1 --net=host --rm --detach ghcr.io/mortenlj/codetanks-sample-groovy tcp://localhost:13337
+docker run --label codetanks=sample-groovy-2 --name sample-groovy-2 --net=host --rm --detach ghcr.io/mortenlj/codetanks-sample-groovy tcp://localhost:13337
+docker run --label codetanks=sample-groovy-3 --name sample-groovy-3 --net=host --rm --detach ghcr.io/mortenlj/codetanks-sample-groovy tcp://localhost:13337
+docker run --label codetanks=sample-rust --name sample-rust --net=host --rm --detach -e SAMPLE_RUST__SERVER_URI=tcp://localhost:13337 ghcr.io/mortenlj/codetanks-sample-rust
 
-docker attach codetanks_server
+docker attach codetanks_server || true
 
 docker container list --filter label=codetanks --quiet | xargs --no-run-if-empty docker container stop
