86357fb57411e5a88da270a2d3f2dcef469e7e39
Morten Lied Johansen <mortenjo@ifi.uio.no>
2023-11-06 09:27:26 +0100

    Introduce dedicated completion events for each command

--- sample-rust/src/main.rs
+++ sample-rust/src/main.rs
@@ -1,8 +1,10 @@
 use anyhow::{anyhow, Result};
+use euclid::default::Vector2D;
 use tokio::{select, signal};
 use tracing::{info, warn};
-use crate::domain::event::Event;
 
+use crate::domain::command::OptionalValue;
+use crate::domain::event::Event;
 use crate::settings::AppConfig;
 
 mod domain;
@@ -16,6 +18,70 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+
+#[derive(Debug)]
+enum State {
+    WaitingForGameStart,
+    Searching,
+    SearchingNarrow,
+    Firing1,
+    Firing2,
+    Aiming,
+    AimingAtTarget,
+    Rotating,
+    RotatingToHunt,
+    Moving,
+}
+
+async fn start_searching(commander: &mut comms::Commander, angle: i32) -> Result<State> {
+    info!("Starting search with angle spread {}", angle);
+    commander.command(domain::Command {
+        r#type: domain::CommandType::Scan as i32,
+        optional_value: Some(OptionalValue::Value(angle)),
+    }).await?;
+    Ok(State::Searching)
+}
+
+
+fn make_vector(point: &Option<domain::Point>) -> Result<Vector2D<f64>> {
+    match point {
+        Some(p) => {
+            Ok(Vector2D::new(p.x, p.y))
+        }
+        None => {
+            Err(anyhow!("point missing"))
+        }
+    }
+}
+
+async fn start_aiming_at_target(commander: &mut comms::Commander, target: &domain::Tank, me: &domain::Tank) -> Result<State> {
+    let my_pos = make_vector(&me.position)?;
+    let target_pos = make_vector(&target.position)?;
+    let target_angle = my_pos.angle_to(target_pos);
+    let my_aim = make_vector(&me.turret)?;
+    let aim_change = my_aim.angle_from_x_axis() - target_angle;
+    start_aiming_at_angle(commander, aim_change.to_degrees() as i32).await?;
+    Ok(State::AimingAtTarget)
+}
+
+async fn start_aiming_at_angle(commander: &mut comms::Commander, angle: i32) -> Result<State> {
+    info!("Adjusting aim by {} degrees", angle);
+    commander.command(domain::Command {
+        r#type: domain::CommandType::Aim as i32,
+        optional_value: Some(OptionalValue::Value(angle)),
+    }).await?;
+    Ok(State::Aiming)
+}
+
+async fn start_firing(commander: &mut comms::Commander) -> Result<State> {
+    info!("FIRE!");
+    commander.command(domain::Command {
+        r#type: domain::CommandType::Fire as i32,
+        optional_value: None,
+    }).await?;
+    Ok(State::Firing1)
+}
+
 #[tokio::main]
 async fn app(config: AppConfig) -> Result<()> {
     logging::init_logging(&config)?;
@@ -24,6 +90,7 @@ async fn app(config: AppConfig) -> Result<()> {
     let repl = comms::register(&config.server_uri).await?;
 
     let mut commander = comms::Commander::new(&repl).await?;
+    let mut current_state = State::WaitingForGameStart;
 
     loop {
         select! {
@@ -36,27 +103,100 @@ async fn app(config: AppConfig) -> Result<()> {
                     Ok(event) => {
                         info!("Received event {:?}", event);
                         if let Some(inner) = &event.event {
-                            info!("Received inner event {:?}", inner);
-                            match inner {
-                                Event::Scan(scan_result) => {
-                                    info!("Scan result: {:?}", scan_result);
+                            info!("Received inner event {:?} while in state {:?}", inner, current_state);
+                            current_state = match current_state {
+                                State::WaitingForGameStart => {
+                                    match inner {
+                                        Event::GameStarted(_) => {
+                                            start_searching(&mut commander, 20).await?
+                                        },
+                                        _ => {
+                                            State::WaitingForGameStart
+                                        },
+                                    }
                                 },
-                                Event::Death(death) => {
-                                    info!("Death: {:?}", death);
+                                State::Searching => {
+                                    match inner {
+                                        Event::ScanComplete(scan_complete) => {
+                                            let me = scan_complete.you.to_owned().unwrap();
+                                            if scan_complete.tanks.len() > 0 {
+                                                let target = scan_complete.tanks[0].to_owned();
+                                                start_aiming_at_target(&mut commander, &target, &me).await?
+                                            } else {
+                                                start_aiming_at_angle(&mut commander, 20).await?
+                                            }
+                                        },
+                                        _ => {
+                                            State::Searching
+                                        },
+                                    }
                                 },
-                                Event::Result(command_result) => {
-                                    info!("Command result: {:?}", command_result);
+                                State::SearchingNarrow => {
+                                    match inner {
+                                        _ => {
+                                            State::SearchingNarrow
+                                        },
+                                    }
                                 },
-                                Event::GameStarted(game_started) => {
-                                    info!("Game started: {:?}", game_started);
+                                State::Firing1 => {
+                                    match inner {
+                                        Event::ShotFired(_) => {
+                                            start_firing(&mut commander).await?
+                                        }
+                                        _ => {
+                                            State::Firing1
+                                        },
+                                    }
                                 },
-                                Event::GameOver(game_over) => {
-                                    info!("Game over: {:?}", game_over);
+                                State::Firing2 => {
+                                    match inner {
+                                        _ => {
+                                            State::Firing2
+                                        },
+                                    }
                                 },
-                                Event::GameData(game_data) => {
-                                    info!("Game data: {:?}", game_data);
+                                State::Aiming => {
+                                    match inner {
+                                        Event::AimingComplete(_) => {
+                                            start_searching(&mut commander, 20).await?
+                                        }
+                                        _ => {
+                                            State::Aiming
+                                        },
+                                    }
+                                }
+                                State::AimingAtTarget => {
+                                    match inner {
+                                        Event::AimingComplete(_) => {
+                                            start_firing(&mut commander).await?
+                                        }
+                                        _ => {
+                                            State::AimingAtTarget
+                                        },
+                                    }
                                 },
-                            }
+                                State::Rotating => {
+                                    match inner {
+                                        _ => {
+                                            State::Rotating
+                                        },
+                                    }
+                                },
+                                State::RotatingToHunt => {
+                                    match inner {
+                                        _ => {
+                                            State::RotatingToHunt
+                                        },
+                                    }
+                                },
+                                State::Moving => {
+                                    match inner {
+                                        _ => {
+                                            State::Moving
+                                        },
+                                    }
+                                },
+                            };
                         }
                     },
                     Err(e) => {
